f8959a40f4a07748dc6e4d06b95398b006dec1a1
flexx84 <bluetank1107@gmail.com>
2025-05-20 19:15:16 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import KimchiPremium from "./components/KimchiPremium";
+
+function App() {
+  return (
+    <div>
+      <KimchiPremium />
+    </div>
+  );
+}
+
+export default App;
